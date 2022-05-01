4b1ab8eb389f2f7f2535daf76baac54bac3a91a9
Conner Foster <connerf99@gmail.com>
2022-04-30 20:09:21 -0400

    CSS added and JS mostly finished

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-let temp;
-let description;
-let feelsLike;
-let wind;
-let humidity;
+const main = document.getElementById("content");
+const searchBtn = document.getElementById("search-btn");
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
 
 async function getWeatherData(location) {
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=486c9beaf76f78b114fb560ce37bf6fa`,
+    `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
     {
       mode: "cors",
     }
   );
-  let data = await response.json();
-  temp = data.main.temp;
-  description = data.weather[0].main;
-  feelsLike = data.main.feels_like;
-  wind = data.wind.speed;
-  humidity = data.main.humidity;
+  if (response.status == 400) {
+    alert("Invalid Location");
+    return;
+  }
 
-  //console.log(data);
+  let data = await response.json();
 
   return data;
 }
 
-const data = getWeatherData("London").then((data) => {
-  console.log(data.main.temp);
+function displayContent(location) {
+  getWeatherData(location).then((data) => {
+    const description = data.current.condition.text;
+    const temp = Math.round(data.current.temp_f);
+    const feelLike = Math.round(data.current.feelslike_f);
+    const wind = Math.round(data.current.wind_mph);
+    const humidity = data.current.humidity;
+    let location = `${data.location.name}, ${data.location.country}`;
+
+    if (data.location.country === "United States of America") {
+      location = `${data.location.name}, ${data.location.region}`;
+    }
+
+    main.innerHTML = `
+        <h2>${description}</h2>
+        <h1>${location.toUpperCase()}</h1>
+        <h1>${temp} &#8457</h1>
+        <h3>FEELS LIKE: ${feelLike} &#8457</h3>
+        <h3>WIND: ${wind} MPH</h3>
+        <h3>HUMIDITY: ${humidity}%</h3>
+        `;
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  displayContent(input.value);
 });
+
+displayContent("London");
