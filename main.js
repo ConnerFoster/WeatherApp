8403197d8d4c4a62b435728cb7945d59a836ae94
Conner Foster <connerf99@gmail.com>
2022-04-30 23:18:18 -0400

    corrected http to https

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const errorText = document.getElementById("err");
 
 async function getWeatherData(location) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
+    `https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
     {
       mode: "cors",
     }
